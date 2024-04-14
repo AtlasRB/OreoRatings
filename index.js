3161e5f3ea7f9a2e9fe2ae114e3afa5c76a0db5c
AtlasRB <connoryeeles@outlook.com>
2024-04-14 14:22:52 +0100

    Added basic structure for homemade oreo milkshake table - Needs work

--- index.js
+++ index.js
@@ -12,3 +12,18 @@ function showOreos(popupId) {
   // Toggle the visibility of the clicked popup
   popup.classList.toggle("show");
 }
+
+function showMilkshakes(popupId) {
+  var popup = document.getElementById(popupId);
+  var popups = document.querySelectorAll('.popuptext');
+
+  // Hide other popups if they are currently shown
+  popups.forEach(function(item) {
+      if (item.id !== popupId && item.classList.contains("show")) {
+          item.classList.remove("show");
+      }
+  });
+
+  // Toggle the visibility of the clicked popup
+  popup.classList.toggle("show");
+}
